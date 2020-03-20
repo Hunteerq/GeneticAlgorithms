a389317a77a94a7ad2ed1c009f2f312106b1916a
Artur Kulig <hunteerq@icloud.com>
2020-03-20 18:29:00 +0100

    Inversion

--- libs/chromosome_modifier.py
+++ libs/chromosome_modifier.py
@@ -3,15 +3,30 @@ import numpy as np
 
 class ChromosomeModifier:
 
-    def __init__(self, inversion_prob, cross_prob):
+    def __init__(self, cross_prob, mutation_prob, inversion_prob):
+        self.__cross_prob = cross_prob
+        self.__mutation_prob = mutation_prob
         self.__inversion_prob = inversion_prob
-        self.cross_prob = cross_prob
 
-    def inversion(self, tab):
-        return np.array([self.__inverse(el) for el in tab])
+    def mutation(self, tab):
+        return np.array([self.__mutate(el) for el in tab])
 
-    def __inverse(self, el):
-        if np.random.random() < self.__inversion_prob:
+    def __mutate(self, el):
+        if np.random.random() < self.__mutation_prob:
             return np.int(not el)
         else:
             return np.int(el)
+
+    def inversion(self, tab):
+        if np.random.random() < self.__inversion_prob:
+            return self.__invert(tab)
+        else:
+            return tab
+
+    def __invert(self, tab):
+        start = np.random.randint(0, len(tab))
+        end = np.random.randint(start, len(tab))
+        tab[start:end] = np.flip(tab[start:end])
+        return tab
+
+
