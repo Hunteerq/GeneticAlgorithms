7b208233100080e655b88666218c56e02a978175
Artur Kulig <hunteerq@icloud.com>
2020-03-20 17:49:23 +0100

    Refactor for np array

--- libs/chromosome_modifier.py
+++ libs/chromosome_modifier.py
@@ -3,15 +3,16 @@ import numpy as np
 
 class ChromosomeModifier:
 
-    def __init__(self, inversion_probability):
-        self.__inversion_probability = inversion_probability
+    def __init__(self, inversion_prob, cross_prob):
+        self.__inversion_prob = inversion_prob
+        self.cross_prob = cross_prob
 
     def inversion(self, tab):
-        return list(map(lambda el: self.__inverse(el), tab))
+        return np.array(list(map(lambda el: self.__inverse(el), tab)))
 
     def __inverse(self, el):
-        if np.random.random() < self.__inversion_probability:
-            return not el
+        if np.random.random() < self.__inversion_prob:
+            return np.int(not el)
         else:
-            return el
+            return np.int(el)
 
