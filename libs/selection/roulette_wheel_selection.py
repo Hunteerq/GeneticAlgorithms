0e82706b453426831dc39bc58956d4bbca882454
Artur Kulig <hunteerq@icloud.com>
2020-04-02 23:59:45 +0200

    Roulette hotfix

--- libs/selection/roulette_wheel_selection.py
+++ libs/selection/roulette_wheel_selection.py
@@ -3,13 +3,16 @@ import numpy as np
 
 class RouletteWheelSelection:
 
-    def __init__(self, function):
+    def __init__(self, function, is_max_opt):
         self.__function = function
+        self.__is_max_opt = is_max_opt
 
     def get_population(self, old_pop):
         old_pop = np.sort(old_pop)
         eval_pop = self.__function.evaluate_population(old_pop)
-        corrected_eval_pop = eval_pop + abs(eval_pop.min()) + 1
+        if not self.__is_max_opt:
+            eval_pop = 1/eval_pop
+        corrected_eval_pop = eval_pop + abs(eval_pop.min())
         wheel = np.cumsum(corrected_eval_pop)
         new_pop = []
         for el in np.random.rand(np.shape(old_pop)[0]):
